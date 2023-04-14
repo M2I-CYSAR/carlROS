32ad7959909a0d21d302e589f2e3e09cb8d6e5a2
ColeHunt <huntcr2001@gmail.com>
2023-04-14 18:46:09 -0500

    Adds DriveTrain class for control

--- driveTrain.py
+++ driveTrain.py
@@ -1,63 +1,69 @@
-#!/usr/bin/python3
+"""
+driveTrain.py
 
-from SparkCAN import SparkBus
+Desc: Method to control drive motors for carl
+Author: Cole Hunt
+Date: 4/13/23
+
+"""
+
+from SparkCANLib import SparkController
 
 # CAN IDs for Drive Controllers
 FLD = 11
-FRD = 21
-RLD = 31
-RRD = 41
+FRD = 12
+RLD = 13
+RRD = 14
 
 # Factor to scale Drive Speed
 SCALE = 0.75
+class DriveTrain:
 
-#Instantiate SparkBus object
-bus = SparkBus(channel="can0", bustype='socketcan', bitrate=1000000)
+    def __init__(self, bus):
+        self.bus = bus
+        # Create a new controllers for Drive Motor Controllers
+        # Front Left Drive SparkMax
+        self.sparkFLD = self.bus.init_controller(FLD)
+        # Front Right Drive SparkMax
+        self.sparkFRD = self.bus.init_controller(FRD)
+        # Rear Left Drive SparkMax
+        self.sparkRLD = self.bus.init_controller(RLD)
+        # Rear Right Drive SparkMax
+        self.sparkRRD = self.bus.init_controller(RRD)
 
-# Create a new controllers for Drive Motor Controllers
-# Front Left Drive SparkMax
-sparkFLD = bus.init_controller(FLD)
-# Front Right Drive SparkMax
-sparkFRD = bus.init_controller(FRD)
-# Rear Left Drive SparkMax
-sparkRLD = bus.init_controller(RLD)
-# Rear Right Drive SparkMax
-sparkRRD = bus.init_controller(RRD)
+    def arcadeDrive(self, rotate, drive):
+        # variables to determine the quadrants
+        maximum = max(abs(drive), abs(rotate)) * SCALE
+        total = (drive + rotate) * SCALE
+        difference = (drive - rotate) * SCALE
 
-def arcade_drive(rotate, drive):
-    """Drives the robot using arcade drive."""
-    # variables to determine the quadrants
-    maximum = max(abs(drive), abs(rotate)) * SCALE
-    total = (drive + rotate) * SCALE
-    difference = (drive - rotate) * SCALE
+        # set speed according to the quadrant that the values are in
+        if drive >= 0:
+            if rotate >= 0:  # I quadrant
+                self.sparkFLD.percent_output(maximum)
+                self.sparkRLD.percent_output(maximum)
 
-    # set speed according to the quadrant that the values are in
-    if drive >= 0:
-        if rotate >= 0:  # I quadrant
-            sparkFLR.percent_output(maximum)
-            sparkRLD.percent_output(maximum)
+                self.sparkFRD.percent_output(difference)
+                self.sparkRRD.percent_output(difference)
 
-            sparkFRR.percent_output(difference)
-            sparkRRD.percent_output(difference)
+            else:            # II quadrant
+                self.sparkFLD.percent_output(total)
+                self.sparkRLD.percent_output(total)
 
-        else:            # II quadrant
-            sparkFLD.percent_output(total)
-            sparkRLD.percent_output(total)
+                self.sparkFRD.percent_output(maximum)
+                self.sparkRRD.percent_output(maximum)
 
-            sparkFRR.percent_output(maximum)
-            sparkRRD.percent_output(maximum)
+        else:
+            if rotate >= 0:  # IV quadrant
+                self.sparkFLD.percent_output(total)
+                self.sparkRLD.percent_output(total)
 
-    else:
-        if rotate >= 0:  # IV quadrant
-            sparkFLD.percent_output(total)
-            sparkRLD.percent_output(total)
+                self.sparkFRD.percent_output(-maximum)
+                self.sparkRRD.percent_output(-maximum)
 
-            sparkFRR.percent_output(-maximum)
-            sparkRRD.percent_output(-maximum)
+            else:            # III quadrant
+                self.sparkFLD.percent_output(-maximum)
+                self.sparkRLD.percent_output(-maximum)
 
-        else:            # III quadrant
-            sparkFLD.percent_output(-maximum)
-            sparkRLD.percent_output(-maximum)
-
-            sparkFRR.percent_output(difference)
-            sparkRRD.percent_output(difference)
+                self.sparkFRD.percent_output(difference)
+                self.sparkRRD.percent_output(difference)
