0eb72b57f6499a674e49eeecab7f4ecc43cc92a1
ColeHunt <huntcr2001@gmail.com>
2023-04-17 01:13:53 -0500

    Integrates new OI into teleop for test

--- teleop.py
+++ teleop.py
@@ -12,54 +12,28 @@ Date: 4/14/23
 from driveTrain import DriveTrain
 from SparkCANLib import SparkCAN
 from flipperControl import FlipperControl
-from GameControllerLib import Gamepad
+from operatorInterface import OI
 import time
 
 ENABLED = True
 
 def main():
 
-    # Gamepad settings
-    gamepadType = Gamepad.Xbox360
-    joystickSpeed = 'LEFT-Y'
-    joystickSteering = 'LEFT-X'
-
-    flipperHome = 'B'
-    flipperRaise ='Y'
-    flipperLower = 'A'
-
-    leftBumper = 'LB'
-    rightBumper = 'RB'
-    leftTrigger = 'LT'
-    rightTrigger = 'RT'
-
-    # Wait for a connection
-    if not Gamepad.available():
-        print('Please connect your gamepad...')
-        while not Gamepad.available():
-            time.sleep(1.0)
-    gamepad = gamepadType()
-    print('Gamepad connected')
+    # Create Operatior Interface object
+    oi = OI()
 
     #Instantiate SparkBus object
-    bus = SparkCAN.SparkBus(channel="can0", bustype='socketcan', bitrate=1000000)
+    #bus = SparkCAN.SparkBus(channel="can0", bustype='socketcan', bitrate=1000000)
 
-    driveTrainObj = DriveTrain(bus)
+    #driveTrainObj = DriveTrain(bus)
     #flipperControlObj = FlipperControl(bus)
 
     steering = 0
     speed = 0
 
     while(ENABLED):
-        eventType, control, value = gamepad.getNextEvent()
-        if control == joystickSpeed:
-            # Speed control (inverted)
-            speed = -value
-        elif control == joystickSteering:
-            # Steering control (not inverted)
-            steering = value
-        #print(f"Speed: {speed}, Steer: {steering}")
-        driveTrainObj.arcadeDrive(speed, steering)
+        print(f"X: {oi.getLeftJoystickXAxis}, Y: {oi.getLeftJoystickYAxis}, A: {oi.getAButtonPressed}, B: {oi.getBButtonPressed}")
+        #driveTrainObj.arcadeDrive(speed, steering)
 
 if __name__ == "__main__":
     main()
